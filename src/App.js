128ac803ef5757cb3513c734c17a6333f80d8390
Jeff Williams <jwill1258@gmail.com>
2020-03-16 23:33:21 -0400

    Initial Routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+//import Huddle from "./pages/Huddle";
+//import Active from "./pages/Active";
+//import Snap from "./pages/Snap";
+//import Profile from "./pages/Profile";
+import Nav from "./components/Nav";
+
 import './App.css';
+import Youtube from './Youtube';
 
 function App() {
   return (
+   
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/youtube" component={Youtube} />
+          
+        </Switch>
+      </div>
+    </Router>
+    
+     
+     
+     
     </div>
   );
 }
